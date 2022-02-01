f59e0fdf66cbcb86251a2702e76939a89079c839
CW <cwainwright26@btinternet.com>
2022-02-01 15:36:26 +0000

    Initial Code Commit

---
+++ src/main.py
@@ -0,0 +1,7 @@
+class Particle:
+    def __init__(self, charge, boson, lepton, mass):
+        self.charge = charge
+        self.boson = boson
+        self.lepton = lepton
+        self.mass = mass
+        
