44a40ef35c800ac07567320bcfaabc1539783e48
CW <cwainwright26@btinternet.com>
2022-02-02 11:53:42 +0000

    Fixed tabular view and created Table class

--- src/tabular_view.py
+++ src/tabular_view.py
@@ -17,7 +17,7 @@ def align(item, max_size, align = "left"):
         return_string += (item + padding * " ")
     elif align == "centre":
         if padding // 2 != 0:
-            padding = [floor(padding//2), ceil(padding//2)]
+            padding = [floor(padding//2), floor(padding//2)+1]
             return_string += (padding[0] * " " + item + padding[1] * " ")
         else:
             return_string += (padding/2 * " " + item + padding/2 * " ")
@@ -27,58 +27,60 @@ def align(item, max_size, align = "left"):
         return_string += (item + padding * " ")
     return return_string
 
-# Functions to construct table features
-def line(max_sizes, line = "mid"):
-    corners = {
-        "top": ["\u250C", "\u252C", "\u2510"],
-        "mid": ["\u251C", "\u253C", "\u2524"],
-        "bot": ["\u2514", "\u2534", "\u2518"]
-    }.get(line, "mid")
-    return_string = "\n" + corners[0]
-    for index, max_size in enumerate(max_sizes):
-        return_string += (max_size)*"\u2500"
-        if 0 < index < len(max_sizes):
-            return_string += corners[1]
-    return_string += corners[2]
-    return return_string
 
-def make_header(headers: list, max_sizes: list) -> str:
-    return_string = "\n"
-    return_string += line(max_sizes, "top")
-    return_string += make_row(headers, max_sizes)
-    return_string += line(max_sizes, "mid")
-    return return_string
+
+
+class Table:
+    def __init__(self, data: list, headers: bool = True, alignment = "left", padding: int = 3):
+        table_data = deepcopy(data)
+        self.return_string = "\n"
+        max_sizes = get_max_sizes(table_data)
+        max_sizes = [max_size + padding for max_size in max_sizes]
+        if headers:
+            self.return_string += self.make_header(table_data[0], max_sizes, alignment)
+            table_data.pop(0)
+        else:
+            self.return_string += self.line(max_sizes, "top")
+        for row in table_data:
+            self.return_string += self.make_row(row, max_sizes, alignment)
+        self.return_string += self.line(max_sizes, "bot")
+        
+    # Functions to construct table features
+    def line(self, max_sizes, line = "mid"):
+        corners = {
+            "top": ["\u250C", "\u252C", "\u2510"],
+            "mid": ["\u251C", "\u253C", "\u2524"],
+            "bot": ["\u2514", "\u2534", "\u2518"]
+        }.get(line, "mid")
+        return_string = "\n" + corners[0]
+        for index, max_size in enumerate(max_sizes):
+            return_string += max_size * "\u2500"
+            if index != len(max_sizes) - 1:
+                return_string += corners[1]
+        return_string += corners[2]
+        return return_string
+
+    def make_header(self, headers: list, max_sizes: list, alignment:str = "left") -> str:
+        return_string = self.line(max_sizes, "top")
+        return_string += self.make_row(headers, max_sizes, alignment)
+        return_string += self.line(max_sizes, "mid")
+        return return_string
+        
+    def make_row(self, data: list, max_sizes: list, alignment: str = "left") -> str:
+        return_string = "\n"
+        for index, item in enumerate(data):
+            return_string += "\u2502" + align(item, max_sizes[index], alignment)
+        return_string += "\u2502"
+        return return_string
     
-def make_row(data: list, max_sizes: list) -> str:
-    return_string = "\n"
-    for index, item in enumerate(data):
-        return_string += "\u2502" + align(item, max_sizes[index])
-    return_string += "\u2502"
-    return return_string
-
-def make_table(data: list, headers: bool = True, padding: int = 3) -> str:
-    table_data = deepcopy(data)
-    return_string = "\n"
-    max_sizes = get_max_sizes(table_data)
-    map(lambda max_size: max_size + padding, max_sizes)
-    if headers:
-        return_string += make_header(table_data[0], max_sizes)
-        table_data.pop(0)
-    else:
-        return_string += line(max_sizes, "top")
-    for row in table_data:
-        return_string += make_row(row, max_sizes)
-    return_string += line(max_sizes, "bot")  
-    return return_string
+    def __str__(self):
+        return self.return_string
 
 if __name__ == "__main__":    
     test_table = [
-        ["This", "is", "a", "header"],
-        ["This", "is", "two", "tests"],
-        ["This", "is", "three", "tests"],
-        ["This", "is", "four", "tests"],
-        ["This", "is", "five", "tests"]
+        ["H1", "H2", "H3"],
+        ["H1.1", "H2.1", "H3.1"],
+        ["H1.2", "H2.2", "H3.2"],
+        ["H1.3", "H2.3", "H3.3"]
     ]
-    print(get_max_sizes(test_table))
-    print(make_table(test_table))
-    print(test_table)
+    print(Table(test_table))
